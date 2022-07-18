c958b7a7c2dd467afa3d63033c2fc440cee9b5dc
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-07-18 18:44:48 +0530

    Queen is working

--- src/com/akshayaap/chess/game/Queen.java
+++ src/com/akshayaap/chess/game/Queen.java
@@ -21,6 +21,122 @@ public class Queen extends Piece{
      */
     @Override
     public void update(){
+        this.resetMap();
 
+        int i = x + 1;
+        int j = y + 1;
+        while (i < 8 && j < 8) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i++;
+            j++;
+        }
+
+        i = x + 1;
+        j = y - 1;
+        while (i < 8 && j >= 0) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        i = x - 1;
+        j = y + 1;
+        while (i >= 0 && j < 8) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i--;
+            j++;
+        }
+
+        i = x - 1;
+        j = y - 1;
+        while (i >= 0 && j >= 0) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i--;
+            j--;
+        }
+
+        i = x + 1;
+        j=y;
+        while (i < 8 ) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i++;
+        }
+
+        i = x - 1;
+        j = y ;
+        while (i  >= 0) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i--;
+        }
+
+        i = x ;
+        j = y + 1;
+        while (j < 8) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            j++;
+        }
+
+        i = x ;
+        j = y - 1;
+        while ( j >= 0) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            j--;
+        }
     }
 }
