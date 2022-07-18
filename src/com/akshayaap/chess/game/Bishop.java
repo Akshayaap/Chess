588059047315c14f4375eff9f10fa06f1598ec0f
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-07-18 18:28:08 +0530

    Bishop is working

--- src/com/akshayaap/chess/game/Bishop.java
+++ src/com/akshayaap/chess/game/Bishop.java
@@ -3,19 +3,79 @@ package com.akshayaap.chess.game;
 /**
  * @Author Akshay
  */
-public class Bishop extends Piece{
+public class Bishop extends Piece {
 
-    public Bishop(int x,int y,Player player){
-        super(x,y,player);
-        this.type=Piece.BISHOP_TYPE;
-        this.value=Piece.BISHOP_VALUE;
+    public Bishop(int x, int y, Player player) {
+        super(x, y, player);
+        this.type = Piece.BISHOP_TYPE;
+        this.value = Piece.BISHOP_VALUE;
     }
 
     /**
      * An Update method
      */
     @Override
-    public  void update(){
+    public void update() {
+        this.resetMap();
 
+        int i = x + 1;
+        int j = y + 1;
+        while (i < 8 && j < 8) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i++;
+            j++;
+        }
+
+        i = x + 1;
+        j = y - 1;
+        while (i < 8 && j >= 0) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        i = x - 1;
+        j = y + 1;
+        while (i >= 0 && j < 8) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i--;
+            j++;
+        }
+
+        i = x - 1;
+        j = y - 1;
+        while (i >= 0 && j >= 0) {
+            if (board[i][j].getPiece() == null) {
+                map[i][j] = true;
+            } else {
+                if (board[i][j].getPiece().getColor() != this.color) {
+                    map[i][j] = true;
+                }
+                break;
+            }
+            i--;
+            j--;
+        }
     }
 }
