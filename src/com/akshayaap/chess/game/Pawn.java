dca7724155a56304999f116c735a12cff71df4bb
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-07-18 17:16:29 +0530

    Pawn is corrected

--- src/com/akshayaap/chess/game/Pawn.java
+++ src/com/akshayaap/chess/game/Pawn.java
@@ -11,62 +11,41 @@ public class Pawn extends Piece{
     public void update(){
         this.resetMap();
         if(this.color){
-            if(this.x==1){
-                if(board[x+1][y].getPiece()==null) {
-                    this.map[x + 1][y] = true;
-                    if (board[x + 2][y].getPiece() == null) {
-                        this.map[x + 2][y] = true;
+            if(x+1<8){
+                if(board[x+1][y].getPiece()==null){
+                    map[x+1][y]=true;
+                    if(x==1&&board[x+2][y].getPiece()==null){
+                        map[x+2][y]=true;
                     }
                 }
-            }else{
-                if(x+1<8){
-                    if (board[x + 1][y].getPiece() == null) {
-                        this.map[x+1][y]=true;
+                if(y+1<8){
+                    if(board[x+1][y+1].getPiece()!=null&&!board[x+1][y+1].getPiece().getColor()){
+                        map[x+1][y+1]=true;
                     }
-                    if(y+1<8){
-                        if(board[x+1][y+1].getPiece()!=null){
-                            if(!board[x+1][y+1].getPiece().getColor()){
-                                this.map[x+1][y+1]=true;
-                            }
-                        }
-                    }
-                    if(y-1>=0){
-                        if(board[x+1][y-1].getPiece()!=null){
-                            if(!board[x+1][y-1].getPiece().getColor()){
-                                this.map[x+1][y-1]=true;
-                            }
-                        }
+                }
+                if(y-1>=0){
+                    if(board[x+1][y-1].getPiece()!=null&&!board[x+1][y-1].getPiece().getColor()){
+                        map[x+1][y-1]=true;
                     }
                 }
             }
-
         }
         else{
-            if(this.x==6){
-                if(board[x-1][y].getPiece()==null) {
-                    this.map[x - 1][y] = true;
-                    if (board[x - 2][y].getPiece() == null) {
-                        this.map[x - 2][y] = true;
+            if(x-1>=0){
+                if(board[x-1][y].getPiece()==null){
+                    map[x-1][y]=true;
+                    if(x==6&&board[x-2][y].getPiece()==null){
+                        map[x-2][y]=true;
                     }
                 }
-            }else{
-                if(x-1>=0){
-                    if (board[x - 1][y].getPiece() == null) {
-                        this.map[x-1][y]=true;
+                if(y+1<8){
+                    if(board[x-1][y+1].getPiece()!=null&&board[x-1][y+1].getPiece().getColor()){
+                        map[x-1][y+1]=true;
                     }
-                    if(y+1<8){
-                        if(board[x-1][y+1].getPiece()!=null){
-                            if(!board[x-1][y+1].getPiece().getColor()){
-                                this.map[x-1][y+1]=true;
-                            }
-                        }
-                    }
-                    if(y-1>=0){
-                        if(board[x-1][y-1].getPiece()!=null){
-                            if(board[x-1][y-1].getPiece().getColor()){
-                                this.map[x-1][y-1]=true;
-                            }
-                        }
+                }
+                if(y-1>=0){
+                    if(board[x-1][y-1].getPiece()!=null&&board[x-1][y-1].getPiece().getColor()){
+                        map[x-1][y-1]=true;
                     }
                 }
             }
